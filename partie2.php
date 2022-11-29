9bcc3e139ac6f4b12bae18a03e5e0a184077ee54
LeMatthieu <matthieublaise3@gmail.com>
2022-11-29 15:14:14 +0100

    exercice pomme

--- partie2.php
+++ partie2.php
@@ -1,21 +1,38 @@
 <?php
- 
- 
- class Chicken {
-    function __clone (){
-        echo "<p> Objet cloner </p>"
-    }
-    function __destruct(){
-        echo "<p> Objet détruit </p>";
-    }
-    function __construct(){
-        echo "<p> objet créer </p>";
 
-    }
 
+ class Pomme {
+   public $nom;
  }
 
- $nugget =new Chicken ();
- $nugget2 =new Chicken ();
+ class Poire {
+    public $nom;
+ }
 
-?>
+$countPoire=0;
+$countpomme=0;
+
+ $pom1 = new Pomme("Pomme");
+ $pom2 = new Pomme("Pomme");
+ $pom3 = new Pomme("Pomme");
+ $pom4 = new Pomme("Pomme");
+ $pom5 = new Pomme("Pomme");
+
+ $poire1 = new Poire("poire");
+ $poire2 = new Poire("poire");
+ $poire3 = new Poire("poire");
+ $poire4 = new Poire("poire");
+
+ $panier = [$pom1, $pom2, $pom3, $pom4,  $pom5, $poire1, $poire2, $poire3, $poire4 ];
+
+ for ($i=0; $i < count($panier); $i++) {
+    if($panier[$i] instanceof Pomme){
+        $countpomme+=1;
+    }
+    elseif($panier[$i] instanceof Poire){
+        $countPoire+=1;
+    }
+ }
+    
+ echo "Votre panier contient " .count($panier)." articles  $countpomme pommes et $countPoire poires";
+ ?>
