547b5446cc32243e905bec5a1b3e180fd3e76b5a
LeMatthieu <matthieublaise3@gmail.com>
2022-11-29 09:42:53 +0100

    creation de ma classe
    creation d'une classe

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
 <?php 
 //creatiopn d'un objet via instanciation
+// class User {
+//     public $nom;
+//     public $age;
+//     function tchater($message) {
+//         echo " - ".$message; 
+//     }
+// }
+// $michel = new User(); // on instancie un objet michel
+// $michel->age=49; //assigne un age
+// $michel->nom="michel"; // oui l'objet michel pas la props
+// echo $michel->tchater("$michel->nom: Salut les dev full stack");
 
-class User {
-    public $nom;
-    public $age;
 
-    function tchater($message) {
-        echo " - ".$message; 
+class job{
+    public $name;
+    public $role;
+    public $Attackrange; 
+
+    function combat($adversaire1, $adversaire2){
+        echo "le combat sera entre $adversaire1 et $adversaire2";
     }
 }
+    $archer = new job();
+    $archer->role="damage dealer";
+    $archer->nom="bowman";
 
-$michel = new User(); // on instancie un objet michel
-$michel->age=49; //assigne un age
-$michel->nom="michel"; // oui l'objet michel pas la props
+    $guerrier = new job();
+    $guerrier->role="tank";
+    $guerrier->nom="warrior";
 
-echo $michel->tchater("$michel->nom: Salut les dev full stack");
-
-
-
-
-
-
-
-
-
-
-
-
+    echo $archer->combat($guerrier->nom, $archer->nom);
 ?>
-
